7b16a5ddb2c593a4bad1d33e49ebf6ad48336d3e
NoveDois <107167514+novedois@users.noreply.github.com>
2022-08-25 19:26:41 -0300

    Add files via upload

---
+++ S02_basico/05_variaveisLet.js
@@ -0,0 +1,17 @@
+/**
+ * Não podemos criar variáveis com palavras reservadas;
+ * Variáveis precisam ter nomes significativos;
+ * Nome da variável não pode começar o nome de uma variável com número;
+ * Nome da variável não pode conter espaços ou traços;
+ * Utilize a notação camelCase
+ * As variáveis são case-sensitive
+ * Não podemos redeclarar variáveis com let
+ * Não utilize var, utilize let
+ */
+let nome = 'João';
+
+console.log(`${nome} nasceu em 1984.`);
+console.log(`Em 2000, ${nome} conheceu Maria.`);
+console.log(`${nome} casou-se com Maria em 2012.`);
+
+
